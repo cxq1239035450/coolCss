1067dc9cf06465d2fdbda73ccf8f45fbe5e947d8
chenxiaoqiang <chenxiaoqiang@dcx.com>
2023-08-23 11:34:41 +0800

    修改three参数

--- src/components/Three/index.vue
+++ src/components/Three/index.vue
@@ -11,32 +11,42 @@ import { nextTick, ref } from 'vue'
  */
 var scene = new THREE.Scene()
 const tween = new TWEEN.Tween({})
+//坐标轴设置
+const axesHelper = new THREE.AxesHelper(30)
+scene.add(axesHelper)
 
 /**
  * 创建网格模型
  */
 var geometry = new THREE.SphereGeometry(50, 32, 16) //创建一个球体几何对象
 
-var material = new THREE.MeshLambertMaterial({}) //受光照影响
-
-const basicMaterial = new THREE.MeshBasicMaterial({
+const material = new THREE.MeshStandardMaterial({
   color: 0xffffff, // 球体的颜色
-  opacity: 0.5, // 设置透明度
+  opacity: 0.1, // 设置透明度
   transparent: true, // 设置为透明材质
-}) //基础材质
+}) //受光照影响
+
 
 //材质对象Material
-var mesh = new THREE.Mesh(geometry, basicMaterial) //网格模型对象Mesh
+var mesh = new THREE.Mesh(geometry, material) //网格模型对象Mesh
 scene.add(mesh) //网格模型添加到场景中
+
+
 /**
  * 光源设置
  */
 //点光源
-var point = new THREE.PointLight(0xffffff)
+const point = new THREE.SpotLight(0xffffff,1)
 point.position.set(50, 50, 100) //点光源位置
-scene.add(point) //点光源添加到场景中
+//平行光
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+
+directionalLight.position.set(100,100,100);
 //环境光
-var ambient = new THREE.AmbientLight(0x444444)
+const ambient = new THREE.AmbientLight(0xffffff,1)
+
+scene.add(point)
+scene.add(directionalLight)
 scene.add(ambient)
 
 /**
@@ -51,8 +61,12 @@ nextTick(() => {
   var k = width / height //窗口宽高比
   var s = 200 //三维场景显示范围控制系数，系数越大，显示的范围越大
   //创建相机对象
-  var camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000)
-  camera.position.set(200, 300, 200) //设置相机位置
+  // var camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000)
+  const camera = new THREE.PerspectiveCamera(30, width / height, 1, 3000)
+  let cameraX = 200
+  let cameraY = 300
+  let cameraZ = 200
+  camera.position.set(cameraX, cameraY, cameraZ) //设置相机位置
   camera.lookAt(scene.position) //设置相机方向(指向的场景对象)
   /**
    * 创建渲染器对象
@@ -63,6 +77,7 @@ nextTick(() => {
   threeBox.value?.appendChild(renderer.domElement)
   //执行渲染操作   指定场景、相机作为参数
   function animate() {
+    camera.position.set(cameraX, cameraY, cameraZ)
     requestAnimationFrame(animate)
     TWEEN.update()
     renderer.render(scene, camera)
